d3b6d84dfa50d1a6d3b7bfef3b56073929b03d49
Chankey Pathak <chankey007@gmail.com>
2019-05-01 20:08:36 +0530

    basic layout ready

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,12 +9,20 @@ import * as moment from 'moment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
   stock = new Stock('', '', '');
   modifiedStock = new Stock('', '', '');
+  minDate: Date;
+  maxDate: Date;
+  isValidDate: boolean;
+  error: string;
 
   constructor(private http: HttpService) {
+    this.minDate = new Date('May 01, 2018 00:00:00');
+    this.maxDate = new Date();
+    this.maxDate.setDate(this.maxDate.getDate() - 1);
   }
 
   onSubmit() {
@@ -22,8 +30,18 @@ export class AppComponent {
     this.modifiedStock.startDate = moment(this.stock.startDate).format('YYYY/MM/DD');
     this.modifiedStock.endDate = moment(this.stock.endDate).format('YYYY/MM/DD');
 
+    this.validateDates(this.modifiedStock.startDate, this.modifiedStock.endDate);
+    if(!this.isValidDate) return;
     this.http.postShareHoldingData(this.modifiedStock).subscribe((data: {}) => {
       console.log(data);
     })
+  }
+
+  validateDates(sDate: string, eDate: string){
+    this.isValidDate = true;
+    if((sDate != null && eDate !=null) && (eDate) <= (sDate)){
+      this.error='End date should be greater than start date.';
+      this.isValidDate = false;
+    }
   }
 }
